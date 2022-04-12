f2313587a911f8d5ddc746cbfc7839168492dd4a
pallavi.raut <pallavi.raut@P3ALAP-1339Y30>
2022-04-12 13:03:16 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sample_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
